710b80c5ee17c2c6c094faa7505ff9a005f188e1
junnys6018 <junkanglim1@gmail.com>
2022-06-08 16:26:24 +0800

    update header

--- docs/components/header.tsx
+++ docs/components/header.tsx
@@ -4,28 +4,12 @@ const Header: React.FC = () => {
     return (
         <header className="border-b">
             <div className="container">
-                <h1 className="mt-10 mb-3 text-4xl font-semibold text-blue-600">Aspen</h1>
-                <div className="flex flex-row">
-                    <h2 className="mr-auto">A toy programming language</h2>
-                    <nav className="flex flex-row gap-1">
-                        <Link href="/">
-                            <a className="text-blue-600 hover:underline">Home</a>
-                        </Link>
-                        <span className="text-blue-600">|</span>
-                        <Link href="/playground">
-                            <a className="text-blue-600 hover:underline">Playground</a>
-                        </Link>
-                        <span className="text-blue-600">|</span>
-                        <a
-                            className="text-blue-600 hover:underline"
-                            href="https://github.com/junnys6018/Aspen"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            GitHub
-                        </a>
-                    </nav>
-                </div>
+                <Link href="/">
+                    <a className="inline-block">
+                        <h1 className="mt-10 mb-3 text-4xl font-semibold text-blue-600">Aspen</h1>
+                    </a>
+                </Link>
+                <h2 className="ml-4 inline-block">A toy programming language</h2>
             </div>
         </header>
     );
