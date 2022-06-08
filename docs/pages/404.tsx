4adc1d223c5dba9019e57a6730685245a0e29d38
junnys6018 <junkanglim1@gmail.com>
2022-06-08 16:36:31 +0800

    custom 404

---
+++ docs/pages/404.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Error404: NextPage = () => {
+    return <h2 className="font-medium text-2xl text-center mt-20">404 - Page Not Found</h2>;
+};
+
+export default Error404;
