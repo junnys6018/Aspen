1f3e78c47adbd1f0ef508cea6a0008c1d108a97e
junnys6018 <junkanglim1@gmail.com>
2022-06-09 23:00:28 +0800

    implemented alert component

---
+++ docs/components/alert.tsx
@@ -0,0 +1,29 @@
+import { FaInfoCircle, FaTimesCircle, FaCheckCircle, FaExclamationTriangle } from 'react-icons/fa';
+
+const Alert: React.FC<{
+    children: React.ReactNode;
+    level: 'info' | 'success' | 'warning' | 'error';
+}> = ({ children, level }) => {
+    const icons = {
+        info: <FaInfoCircle className="mt-[0.3rem] flex-shrink-0" />,
+        success: <FaCheckCircle className="mt-[0.3rem] flex-shrink-0 text-green-600" />,
+        warning: <FaExclamationTriangle className="mt-[0.3rem] flex-shrink-0 text-amber-600" />,
+        error: <FaTimesCircle className="mt-[0.3rem] flex-shrink-0 text-red-600" />,
+    };
+
+    const classNames = {
+        info: 'bg-white border',
+        success: 'bg-green-100',
+        warning: 'bg-amber-100',
+        error: 'bg-red-100',
+    };
+
+    return (
+        <div className={`flew-row not-prose flex gap-2 rounded-md p-3 ${classNames[level]}`}>
+            {icons[level]}
+            <div>{children}</div>
+        </div>
+    );
+};
+
+export default Alert;
